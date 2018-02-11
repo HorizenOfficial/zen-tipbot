de9c01024451fd0de33c53eac3ff48561eff33e4
Lukas Bures <burylukas@seznam.cz>
2018-02-11 18:41:17 +0100

    users fixes

--- bot/modules/tipbot.js
+++ bot/modules/tipbot.js
@@ -432,15 +432,15 @@ function doOpenTip(message, words) {
 
             for (let i = 0; i < tipAllChannels[idx].used_user_id.length; i++) {
                 if (tipAllChannels[idx].used_user_id[i] === receiver.discordID) {
-                    user.sendMessage("<@" + receiver.discordID + "> No, you can NOT 'open' this for the second time ... ");
+                    user.send("<@" + receiver.discordID + "> No, you can NOT 'open' this for the second time ... ");
                     return message.reply("No, you can NOT 'open' this for the second time ... ");
                 }
             }
 
             sendZen(tipper, receiver, amount);
 
-            tipper.sendMessage("<@" + receiver.discordID + "> received your tip (" + amount.toString() + " ZEN)!");
-            receiver.sendMessage("<@" + tipper.discordID + "> sent you a **" + amount.toString() + " ZEN** tip !");
+            tipper.send("<@" + receiver.discordID + "> received your tip (" + amount.toString() + " ZEN)!");
+            receiver.send("<@" + tipper.discordID + "> sent you a **" + amount.toString() + " ZEN** tip !");
 
             tipAllChannels[idx].n_used += 1;
             tipAllChannels[idx].used_user_id.push(receiver.discordID);
