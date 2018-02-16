34866925c3be8df46a7cad541c2c0750435c7a6b
Lukas Bures <burylukas@seznam.cz>
2018-02-16 19:23:40 +0100

    just for debug - get everyone ID 3 fix

--- bot/modules/tipbot.js
+++ bot/modules/tipbot.js
@@ -399,7 +399,7 @@ function doOpenTip(message, receiver, words, bot) {
     console.log("open myRole message.guild.id: ", message.guild.id);
     let myRole = message.guild.roles.get(message.guild.id);
     console.log("open myRole  everyone: ", myRole);
-    console.log("open message.member.roles", message.member.roles);
+    // console.log("open message.member.roles", message.member.roles);
 
     if (message.channel.type === "dm") {
         return message.reply("you can't send me this command in direct message!");
