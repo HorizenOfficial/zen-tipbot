8df3e8ee54745e80333b15c46e5bc03c55233e20
Lukas Bures <burylukas@seznam.cz>
2018-02-23 09:13:03 +0100

    debug 2

--- bot/modules/tipbot.js
+++ bot/modules/tipbot.js
@@ -772,9 +772,9 @@ function doTip(message, tipper, words, bot) {
             console.log("doTip targetId", targetId);
         }
 
-        let foo = bot.users.get(targetId);
+        let foo_user = bot.fetchUser(targetId);
         if (config_bot.debug) {
-            console.log("doTip foo.id", foo.id);
+            console.log("doTip foo_user.id", foo_user.id);
         }
 
         let target = message.guild.members.get(targetId);
